e0faf66ebf597094d6be706b07bd9ea9fc6183fb
camazog1 <carlosm@HPPavilion.>
2025-05-13 12:42:32 -0500

    automation of dollars

--- NEXO/apps/product/models.py
+++ NEXO/apps/product/models.py
@@ -9,15 +9,16 @@ class Product(models.Model):
     reference = models.CharField(max_length=100, primary_key=True)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)  # Precio en COP
-    price_dolar = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)  # Precio en USD
+    price_dolar = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True, editable=False)  # Precio en USD
     is_popular = models.BooleanField(default=False)
     is_new = models.BooleanField(default=False)
     discontinued = models.BooleanField(default=False)
 
     def save(self, *args, **kwargs):
-        if self.price and not self.price_dolar:
+        if self.price:
             self.price_dolar = self.price * EXCHANGE_RATE
         super().save(*args, **kwargs)
+
 class ProductImage(models.Model):
     product = models.ForeignKey(Product, related_name='images', on_delete=models.CASCADE)
     image = models.ImageField(upload_to=product_image_upload_path)
