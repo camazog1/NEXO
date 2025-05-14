41686e387e1dfc32bd2a302cd3ee4b50dfdef6ae
saldaf10 <psaldarrif@eafit.edu.co>
2025-05-13 19:02:06 -0500

    cambios para el despliegue

--- NEXO/config/settings.py
+++ NEXO/config/settings.py
@@ -82,13 +82,17 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'NEXO',
+        'USER': 'admin',
+        'PASSWORD': 'NEXO-Key',
+        'HOST': 'nexo-db.cx6agi2skyby.us-east-1.rds.amazonaws.com',
+        'PORT': '3306',
     }
 }
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')  # Local media path
+MEDIA_ROOT = '/mnt/efs/media/'  # Ruta en el sistema de archivos EFS de AWS
 
 AUTH_USER_MODEL = 'users.CustomUser'
 
